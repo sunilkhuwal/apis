f34dbd64b516f9b6c13896a7032286f7ea7e5c1b
Sunil Kumar <sunil.kumar@metacube.com>
2018-02-22 02:43:28 +0530

    adding apis

--- first_request.js
+++ first_request.js
@@ -1,7 +1,19 @@
-var request = require('request');
-
-request('http://google.com', function(error, response, body){
-	if(!error && response.statusCode == 200){
+/*var request = require("request");
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
+	if(!error && response.statusCode ==200){
 		console.log(body);
 	}
 });
+
+*/
+
+console.log("Sunset at Jaipur is ...");
+var request = require("request");
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22jaipur%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
+	if(!error && response.statusCode ==200){
+		var parsedData = JSON.parse(body);
+		console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
+	}
+});
+
+//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
---
+++ movie_search_app/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("search");
+});
+
+app.get("/results", function(req, res){
+	var query = req.query.moviename;
+	var url = "http://www.omdbapi.com/?apikey=2ae1f7ec&s=" + query;
+	console.log(query);
+	request(url, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("results", {data : data});
+			//res.send(result["Search"][0]["Title"]);	
+		}		
+	});
+});
+
+app.listen("3000", function(){
+	console.log("Movie App server started");
+}); 
